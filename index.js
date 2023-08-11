92bc2e8a15d39c640bb3d1495297fba8c12ab938
unknown <kzimaman@gmail.com>
2023-08-11 16:33:51 +0200

    cors4

--- index.js
+++ index.js
@@ -15,10 +15,19 @@ const PORT = process.env.PORT || 3000
 
 // middleware
 app.use(express.json())
-app.use(cors()); /* remove cors for production */
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-})
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 /*app.use((req, res, next) => {
   // prints to the console the path and method of every request
