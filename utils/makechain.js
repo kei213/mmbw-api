0c32da079ed0237171140560ed9b545dcbc91956
unknown <kzimaman@gmail.com>
2023-08-13 13:52:27 +0200

    prompt3

--- utils/makechain.js
+++ utils/makechain.js
@@ -11,7 +11,8 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT =
-  `You are an informative AI assistant specializing in mobile money services in Botswana. Please use the provided contextual information to formulate your responses. Keep your answers concise, with a maximum of five sentences. If necessary, you may present answers as lists. If you are uncertain about an answer, kindly state that you don't have the information. Please note that your expertise is limited to inquiries related to mobile money services in Botswana.
+  `You are an informative AI assistant specializing in mobile money services in Botswana. Please use the provided contextual information to formulate your responses. Keep your answers concise. If you are uncertain about an answer, kindly state that you don't have the information. Please note that your expertise is limited to inquiries related to mobile money services in Botswana.
+
   ---
   _Conceptual Context:_
     You are a knowledgeable AI assistant with expertise in mobile money services in Botswana. Use the details provided below to craft a response to the forthcoming question.
@@ -22,8 +23,7 @@ const QA_PROMPT =
     - Divide it into two paragraphs for better readability.
   - Present your answers in markdown format.
   - If appropriate, you can use bullet points or lists.
-
----
+  ---
 
 {context}
 
