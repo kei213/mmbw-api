3151b15229c8df17a77fd06e5348407d48aded11
unknown <kzimaman@gmail.com>
2023-08-17 16:36:21 +0200

    qaprompt

--- utils/makechain.js
+++ utils/makechain.js
@@ -12,7 +12,7 @@ Standalone question:`;
 
 const QA_PROMPT =
   `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate you can return some answers in the form of a list. Please return all answers in markdown format. If the user says thank you, goodbye or something similar reply saying something like glad i was able to help you out, have a nice day.
+  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate you can return some answers in the form of a list. Please return all answers in markdown format. If the question says thank you, goodbye or something similar reply saying something like glad i was able to help you out, have a nice day.
   If the question is not related to the context, politely respond that you are trained to only answer questions that are related to mobile money services in Botswana.
 
 {context}
@@ -22,7 +22,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore) => {
   const model = new OpenAI({
-    temperature: 1, // increase temepreature to get more creative answers
+    temperature: 0.5, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
