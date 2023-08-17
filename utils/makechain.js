e63359bf985b17f6c34dbe1825ae45d38c465a6d
unknown <kzimaman@gmail.com>
2023-08-17 16:23:36 +0200

    tuning

--- utils/makechain.js
+++ utils/makechain.js
@@ -12,8 +12,8 @@ Standalone question:`;
 
 const QA_PROMPT =
   `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate you can return some answers in the form of a list. Please return all answers in markdown format.
-  If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to mobile money services in Botswana.
+  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate you can return some answers in the form of a list. Please return all answers in markdown format. If the user says thank you, goodbye or something similar reply saying something like glad i was able to help you out, have a nice day.
+  If the question is not related to the context, politely respond that you are trained to only answer questions that are related to mobile money services in Botswana.
 
 {context}
 
