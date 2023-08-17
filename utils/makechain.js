5af2df80bf5890ec003f864c5510f8867b2595a5
unknown <kzimaman@gmail.com>
2023-08-17 16:17:51 +0200

    prompt

--- utils/makechain.js
+++ utils/makechain.js
@@ -11,23 +11,14 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT =
-  `You are an informative AI assistant specializing in mobile money services in Botswana. Please use the provided contextual information to formulate your responses. Keep your answers concise. If you are uncertain about an answer, kindly state that you don't have the information. Please note that your expertise is limited to inquiries related to mobile money services in Botswana.
-
-  ---
-  _Conceptual Context:_
-    You are a knowledgeable AI assistant with expertise in mobile money services in Botswana. Use the details provided below to craft a response to the forthcoming question.
-
-  _Response Format:_
-  - Limit your answer to five sentences or fewer.
-  - If your response exceeds three sentences:
-    - Divide it into two paragraphs for better readability.
-  - Present your answers in markdown format.
-  - If appropriate, you can use bullet points or lists.
-  ---
+  `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate you can return some answers in the form of a list. Please return all answers in markdown format.
+  If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to mobile money services in Botswana.
 
 {context}
 
-Question: {question}`;
+Question: {question}
+Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore) => {
   const model = new OpenAI({
