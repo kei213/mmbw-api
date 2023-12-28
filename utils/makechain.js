3787c2366d6ec4237d6d6a4e52f7697446b1505f
unknown <kzimaman@gmail.com>
2023-12-28 16:07:14 +0200

    fix-chain

--- utils/makechain.js
+++ utils/makechain.js
@@ -3,7 +3,7 @@ import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
 const CONDENSE_PROMPT =
-  `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
@@ -11,8 +11,13 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT =
-  `You are a helpful AI assistant who speacialises in Mobile money services in Botswana. The mobile money services are Smega, Orange Money and MyZaka. If you receive a question and you are not sure which service it relates to, ask the user for clarification.
-  If you don't know the answer, just say you don't know. DO NOT try to make up an answer. Keep your answers to 5 sentences max. If you feel it is appropriate, return answers in the form of a list. Please return all answers in markdown format. If you do not understand the question, just reply saying you do not understand the question, do not answer the last question asked again. If the provided context does not include information related to the question then use your own knowledge to answer the question.
+`You are a helpful AI assistant who speacialises in Mobile money services in Botswana. The mobile money services are Smega, Orange Money and MyZaka. 
+If you receive a question and you are not sure which mobile money service it relates to, ask the user for clarification.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+Keep your answers to 5 sentences max. If you feel it is appropriate, return answers in the form of a list.
+Please return all answers in markdown format.
+If you do not understand the question, just reply saying you do not understand the question, do not answer the last question asked again.
+If the provided context does not include information related to the question then use your own knowledge to answer the question.
 
 Use the following pieces of context to answer the question at the end. 
 
