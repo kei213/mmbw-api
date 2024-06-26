1dada19e7f39f7dde7b031280127c02d1358157f
unknown <kzimaman@gmail.com>
2024-06-26 16:51:19 +0200

    err

---
+++ config/cors-config.js
@@ -0,0 +1,16 @@
+// cors setup
+const developmentOrigins = ["http://localhost:3000"];
+const productionOrigins = ["https://www.mobilemoneybw.co.bw"]
+
+const allowlist = process.env.NODE_ENV === "production" ? productionOrigins : developmentOrigins;
+
+export const corsConfig = {
+    origin: function (origin, callback) {
+      if (allowlist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS not allowed'));
+      }
+    },
+    credentials: true,  
+}
